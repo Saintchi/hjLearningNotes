e721891fb461551add1481897d82cabb81fe9b4c
Saintchi <shengzhipan@qq.com>
2017-08-13 21:01:17 +0800

    story: LearningNotes dts: none author：Hyunji description：LearningNotes

--- src/main/java/com/hyunji/learning/script/groovy/GroovyCache.java
+++ src/main/java/com/hyunji/learning/script/groovy/GroovyCache.java
@@ -16,17 +16,28 @@ public final class GroovyCache {
 	}
 
 	public static synchronized GroovyCache getInstance() {
-		//判断存储实例的变量是否有值
+		// 判断存储实例的变量是否有值
 		if (uniqueInstance == null) {
-			//如果没有，就创建一个类实例，并把值赋值给存储类实例的变量
+			// 如果没有，就创建一个类实例，并把值赋值给存储类实例的变量
 			uniqueInstance = new GroovyCache();
 		}
-		//如果有值，那就直接使用
+		// 如果有值，那就直接使用
 		return uniqueInstance;
 	}
 
 	public Class<?> getClassByScript(String script) {
 
+		String hashKey = "";
+
+		Class<?> groovyCls = null;
+		
+		
+		// 空判断条件
+		putClass(hashKey, groovyCls);
 		return null;
+	}
+
+	private void putClass(String hashKey, Class<?> groovyCls) {
+		GROOVY_CLS_CACHE.put(hashKey, groovyCls);
 	}
 }
